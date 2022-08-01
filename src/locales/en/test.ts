dbf28e1aae59f3856d2e85315b5667c5e68d22e5
Jiafeng Zhou <jiafengzhou@JiafengdeMacBook-Air.local>
2022-08-01 14:28:35 +0800

    feat: 增加测试文件

---
+++ src/locales/en/test.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: zhoujiafeng
+ * @Date: 2022-08-01 14:28:04
+ * @LastEditTime: 2022-08-01 14:28:06
+ * @FilePath: /translate-tool/src/locales/en/test.ts
+ * @LastEditors: 
+ */
+const l = {
+  '111': '222'
+}
+
+export default l
---
+++ src/locales/zh/test.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: zhoujiafeng
+ * @Date: 2022-08-01 14:27:37
+ * @LastEditTime: 2022-08-01 14:27:56
+ * @FilePath: /translate-tool/src/locales/zh/text.ts
+ * @LastEditors: 
+ */
+const l = {
+  '111': '222'
+}
+
+export default l
